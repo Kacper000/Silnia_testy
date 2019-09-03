be680825fcb425d59683e886ca18f9608ac395c2
NorthDeer <kacperdaniel000@gmail.com>
2019-09-03 13:18:52 +0200

    Ukończenie Projektu Silnia_testy

--- src/Silnia.java
+++ src/Silnia.java
@@ -5,12 +5,31 @@ public class Silnia {
         this.liczba = liczba;
     }
 
-    Double ObliczSilnia(){
+    /*Fraza "nie istnieje" jest zastąpiona przez zmienną X=0,
+     ponieważ nie umiem umieścić String w funkcji Double
+     ale chociaż DZIAŁA*/
 
-        for(double i=1; i<=liczba; i++)
-        {
-            liczba=liczba*i;
-        }
-        return liczba;
+    /*Ponadto nie wiem dlaczego test1 sie nie odpala tzn.
+    nie może się dokonać zupełnie jakby miał robić
+    to w nieskończoność. Double.MAX_VALUE nie dawałem
+    bo komputer uległby autodestruckji*/
+
+    /*POZDRO 600*/
+
+    Double ObliczSilnia(){
+        double X=0;
+        double j=0;
+            while(j<liczba) {
+                j++;
+                if (liczba == j) {
+                    for (double i = 1; i <= liczba; i++) {
+                        liczba = liczba * i;
+                    }
+                    return liczba ;
+                }
+                if(liczba<j)
+                    return X;
+            }
+            return X ;
     }
 }
--- src/SilniaTest.java
+++ src/SilniaTest.java
@@ -5,9 +5,57 @@ import org.junit.jupiter.api.Test;
 class SilniaTest {
 
     @Test
-    public void  test1 () {
+    public void test1 () {
         Silnia liczba =new Silnia (3);
         Assertions.assertEquals(liczba.ObliczSilnia(),6 );
 
     }
+    @Test
+    public void  test2 () {
+        Silnia liczba =new Silnia (0);
+        Assertions.assertEquals(liczba.ObliczSilnia(),0 );
+
+    }
+    @Test
+    public void  test3 () {
+        Silnia liczba =new Silnia (0.5);
+        Assertions.assertEquals(liczba.ObliczSilnia(),0);
+
+    }
+    @Test
+    public void  test4 () {
+        Silnia liczba =new Silnia (-1);
+        Assertions.assertEquals(liczba.ObliczSilnia(),0 );
+
+    }@Test
+    public void  test5 () {
+        Silnia liczba =new Silnia (-0.000000000002);
+        Assertions.assertEquals(liczba.ObliczSilnia(),0);
+
+    }
+    @Test
+    public void  test6 () {
+        Silnia liczba =new Silnia (0.00000000000002);
+        Assertions.assertEquals(liczba.ObliczSilnia(),0);
+
+    }
+    @Test
+    public void  test7 () {
+        Silnia liczba =new Silnia (-0.02);
+        Assertions.assertEquals(liczba.ObliczSilnia(),0);
+
+    }
+    @Test
+    public void  test8 () {
+        Silnia liczba =new Silnia (3.5);
+        Assertions.assertEquals(liczba.ObliczSilnia(),0 );
+
+    }
+    @Test
+    public void  test9 () {
+        Silnia liczba =new Silnia (-4.5);
+        Assertions.assertEquals(liczba.ObliczSilnia(),0 );
+
+    }
+
 }
